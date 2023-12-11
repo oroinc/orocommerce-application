2284f787afc3ad3f1ef0c9977ab129ea9cbce291
Maksym Maksymenko <mmaksymenko@oroinc.com>
2023-12-11 11:47:21 +0200

    Added User Naming Index Action

---
+++ src/Training/Bundle/UserNamingBundle/Controller/UserNamingController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Training\Bundle\UserNamingBundle\Controller;
+
+use Training\Bundle\UserNamingBundle\Entity\UserNamingType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Contains CRUD actions for User Naming
+ *
+ * @Route("/naming", name="user_naming")
+ */
+class UserNamingController
+{
+    /**
+     * @Route("/", name="user_naming_index")
+     * @Template
+     *
+     * @return array
+     */
+    public function indexAction(): array
+    {
+        return [
+            'entity_class' => UserNamingType::class
+        ];
+    }
+}
